d727119ce27805f7e5d0bcd715503e2b6f43a466
BanceDev <lanceaborden@protonmail.com>
2024-10-23 19:56:15 -0400

    fixed tag size values

--- RobotCode/Calibration/cameraTest.py
+++ RobotCode/Calibration/cameraTest.py
@@ -7,7 +7,7 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
  
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:7].T.reshape(-1,2)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
  
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
